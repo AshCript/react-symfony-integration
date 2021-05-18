55118c8d39bc0269fb9a0733e1504b86a0b23b31
As Manjaka Josvah <asjosvah@gmail.com>
2021-05-18 17:47:04 +0300

    Integration of Semantic-ui and some tests

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import '../styles/bootstrap.scss';
+// import 'bootstrap/dist/js/bootstrap.bundle';
+import 'semantic-ui-css/semantic.min.css';
+// import 'jquery';
 import App from './components/App';
-
+import Top from './components/Top';
 ReactDOM.render(
   (
-    <App/>
+    <>
+      {/* <Top/> */}
+      {/* <br/> */}
+      <App/>
+    </>
   ),
-  document.querySelector("#app") 
+  document.querySelector("#app")
 );
--- assets/js/components/App.js
+++ assets/js/components/App.js
@@ -1,11 +1,15 @@
-import React from 'react'
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Input, Modal } from 'semantic-ui-react';
 class App extends React.Component{
   render(){
     return (
-      <div className="container">
-        <h1 className="btn btn-primary">Hello World</h1>
-      </div>
+      <Modal
+      trigger={<Button>Show Modal</Button>}
+      header='Reminder!'
+      content='Call Benjamin regarding the reports.'
+      actions={['Snooze', { key: 'done', content: 'Done', positive: true }]}
+    />
     )
   }
 }
